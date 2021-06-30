7809c42f004d3a410d442c9c8dfea43f5baefa2a
Vardhah Anees <63852903+vardhahanees@users.noreply.github.com>
2021-06-30 23:05:35 +0400

    Update ari.c

---
+++ ari.c
@@ -0,0 +1,57 @@
+#include<conio.h>
+#include<stido.h>
+string ari(string s)
+{
+  int characters=0;
+  int words=0;
+  int sentences=0;
+  for (int i=0; s[i]!=/0; i++)
+  {
+      if (isalnum(s[i])==1)
+      {
+         characters++;
+      }
+    else if (s[i]==" ")
+    {
+       words++;
+    }
+    else if (s[i]=='.' || s[i]=="!" || s[i]=="?")
+    {
+       sentences++;
+    }
+  }
+  int ari=0;
+  string g;
+  ari = (4.71 * (characters/words) + 0.5 * (words/sentences) - 21.43); 
+  if score==1
+    g="Kindergaten";
+  else if score==2
+    g="First/Second Grade";
+  else if score==3
+    g="Third Grade";
+  else if score==4
+    g="Fourth Grade";
+  else if score==5
+    g="Fifth Grade";
+  else if score==6
+    g="Sixth Grade";
+  else if score==7
+    g="Seventh Grade";
+  else if score==8
+    g="Eighth Grade";
+  else if score==9
+    g="Ninth Grade";
+  else if score==10
+    g="Tenth Grade";
+  else if score==11
+    g="Eleventh Grade";
+  else if score==12
+    g="Twelfth Grade";
+  else if score==13
+    g=="College student";
+  return(g);
+}
+  
+    
+    
+     
