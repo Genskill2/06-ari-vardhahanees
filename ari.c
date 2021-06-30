d02afd2337081e5edeb3d7c5ed7840fdc2c0b9fc
Vardhah Anees <63852903+vardhahanees@users.noreply.github.com>
2021-06-30 23:19:22 +0400

    Update ari.c

--- ari.c
+++ ari.c
@@ -18,40 +18,39 @@ string ari(string s)
     {
        words++;
     }
-    else if (s[i]=='.' || s[i]=="!" || s[i]=="?")
+    else if (s[i]=='.' || s[i]=='!' || s[i]=='?')
     {
-     
       sentences++;
     }
   }
-  int ari=0;
+  int score=0;
   string g;
-  ari = (4.71 * (characters/words) + 0.5 * (words/sentences) - 21.43); 
-  if score==1
+  score = (4.71 * (characters/words) + 0.5 * (words/sentences) - 21.43); 
+  if (score==1)
     g="Kindergaten";
-  else if score==2
+  else if (score==2)
     g="First/Second Grade";
-  else if score==3
+  else if (score==3)
     g="Third Grade";
-  else if score==4
+  else if (score==4)
     g="Fourth Grade";
-  else if score==5
+  else if (score==5)
     g="Fifth Grade";
-  else if score==6
+  else if (score==6)
     g="Sixth Grade";
-  else if score==7
+  else if (score==7)
     g="Seventh Grade";
-  else if score==8
+  else if (score==8)
     g="Eighth Grade";
-  else if score==9
+  else if (score==9)
     g="Ninth Grade";
-  else if score==10
+  else if (score==10)
     g="Tenth Grade";
-  else if score==11
+  else if (score==11)
     g="Eleventh Grade";
-  else if score==12
+  else if (score==12)
     g="Twelfth Grade";
-  else if score==13
+  else if (score==13)
     g=="College student";
   return(g);
 }
